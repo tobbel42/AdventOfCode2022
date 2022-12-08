172a0ddec3f61df500dfb673647c20180b4acbe1
tobbel42 <tobiasdegrossmann@gmx.de>
2022-12-08 12:33:58 +0100

    done day07

---
+++ day07/day07.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+struct Node {
+  std::string name;
+  size_t size;
+  bool isDir;
+  Node * parent;
+  std::vector<Node *> children;
+};
+
+Node *newNode(std::string name, size_t size, bool isDir, Node * parent){
+  Node * tmp = new Node;
+  tmp->name = name;
+  tmp->size = size;
+  tmp->isDir = isDir;
+  tmp->parent = parent;
+  return tmp;
+}
+
+std::vector<std::string> splitLine(const std::string & line){
+  std::vector<std::string> tmp;
+  size_t start = 0;
+  size_t end = line.find(' ', 0);
+  while (true) {
+    tmp.push_back(line.substr(start, end - start));
+    if (end == std::string::npos)
+      break;
+    start = end + 1;
+    end = line.find(' ', start);
+  }
+  return tmp;
+}
+
+void printTree(Node * node, int depth){
+  for (int i = 0; i <  node->children.size(); ++i){
+    for (int j = 0; j < depth; ++j)
+      std::cout << " ";
+    std::cout << "- " << node->children[i]->name 
+      << " (" << (node->children[i]->isDir?"dir ":"file ")
+      <<  "size=" << node->children[i]->size << ")" << std::endl;
+    if (node->children[i]->isDir)
+      printTree(node->children[i], depth + 1);
+  }
+}
+size_t getSize(Node * node, size_t & sum){
+  size_t tmp = 0;
+  for (int i = 0; i <  node->children.size(); ++i){
+    if (node->children[i]->isDir)
+      node->children[i]->size = getSize(node->children[i], sum);
+    tmp += node->children[i]->size;
+  }
+  if (tmp <= 100000)
+    sum += tmp;
+  return tmp;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2){
+    std::cerr << "Incorrect number of arguments!" << std::endl;
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+
+  if (!input.is_open()) {
+    std::cerr << "Can't open file!" << std::endl; 
+    return 1;
+  }
+
+  std::string line;
+  std::vector<std::string> args;
+
+  std::getline(input, line);
+  Node * head = newNode("/", 0, true, NULL);
+  Node * node = head;
+
+  while (std::getline(input, line))
+  {
+    args = splitLine(line);
+    if (args[0] == "$"){
+      if (args[1] == "js")
+        continue;
+      else if (args[1] == "cd") {
+        if (args[2] == "..")
+          node = node->parent;
+        else
+        {
+          for (size_t i = 0;  i < node->children.size(); ++i) {
+            if (node->children[i]->name == args[2])
+              node = node->children[i];
+          }
+        }
+      }
+    }
+    else{
+      if (args[0] == "dir")
+        node->children.push_back(newNode(args[1], 0, true, node));
+      else 
+        node->children.push_back(newNode(args[1], std::stoi(args[0]), false, node));
+    }
+  }
+
+  size_t sum = 0;
+
+  getSize(head, sum);
+
+  std::cout << sum << std::endl;
+  
+  return 0;
+}
